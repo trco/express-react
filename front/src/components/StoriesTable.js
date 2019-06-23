05c592d7cb87c7ce07f63859a3793620f3c00742
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-06-24 00:13:56 +0200

    update pagination and css

--- front/src/components/StoriesTable.js
+++ front/src/components/StoriesTable.js
@@ -5,8 +5,44 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 
 import StoryModal from './StoryModal';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+
+const customTotal = (from, to, size) => (
+  <span className="react-bootstrap-table-pagination-total" style={{marginLeft: '1em'}}>
+    Showing { from } to { to } of { size } stories.
+  </span>
+);
+
+const options = {
+  paginationSize: 5,
+  pageStartIndex: 1,
+  // alwaysShowAllBtns: true, // Always show next and previous button
+  // withFirstAndLast: false, // Hide the going to First and Last page button
+  // hideSizePerPage: true, // Hide the sizePerPage dropdown always
+  // hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
+  firstPageText: 'First',
+  prePageText: 'Back',
+  nextPageText: 'Next',
+  lastPageText: 'Last',
+  nextPageTitle: 'First page',
+  prePageTitle: 'Pre page',
+  firstPageTitle: 'Next page',
+  lastPageTitle: 'Last page',
+  showTotal: true,
+  paginationTotalRenderer: customTotal,
+  sizePerPage: 10,
+  sizePerPageList: [{
+    text: '5', value: 5
+  }, {
+    text: '10', value: 10
+  }, {
+    text: '25', value: 25
+  }, {
+    text: '50', value: 50
+  }]
+};
 
 class StoriesTable extends React.Component {
 
@@ -69,7 +105,7 @@ class StoriesTable extends React.Component {
             keyField='id'
             data={stories}
             columns={columns}
-            pagination={paginationFactory()}
+            pagination={paginationFactory(options)}
             bootstrap4={true}
           />
           <StoryModal
