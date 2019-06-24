4c4a639ecbb7bb98f92e5b4a86c44672a74230b8
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-06-24 14:42:57 +0200

    refactor pagination, table and fix modal

--- front/src/components/StoriesTable.js
+++ front/src/components/StoriesTable.js
@@ -1,61 +1,27 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-// import matchSorter from 'match-sorter';
 
 import StoryModal from './StoryModal';
+import {paginationOptions} from './settings';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
-const customTotal = (from, to, size) => (
-  <span className="react-bootstrap-table-pagination-total" style={{marginLeft: '1em'}}>
-    Showing { from } to { to } of { size } stories.
-  </span>
-);
-
-const options = {
-  paginationSize: 5,
-  pageStartIndex: 1,
-  // alwaysShowAllBtns: true, // Always show next and previous button
-  // withFirstAndLast: false, // Hide the going to First and Last page button
-  // hideSizePerPage: true, // Hide the sizePerPage dropdown always
-  // hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
-  firstPageText: 'First',
-  prePageText: 'Back',
-  nextPageText: 'Next',
-  lastPageText: 'Last',
-  nextPageTitle: 'First page',
-  prePageTitle: 'Pre page',
-  firstPageTitle: 'Next page',
-  lastPageTitle: 'Last page',
-  showTotal: true,
-  paginationTotalRenderer: customTotal,
-  sizePerPage: 10,
-  sizePerPageList: [{
-    text: '5', value: 5
-  }, {
-    text: '10', value: 10
-  }, {
-    text: '25', value: 25
-  }, {
-    text: '50', value: 50
-  }]
-};
-
 class StoriesTable extends React.Component {
 
   state = {
+    // modal
+    modalIsOpen: false,
+    modalContent: [],
     // table
     isLoading: true,
     stories: [],
-    // modal
-    modalIsOpen: false,
-    modalContent: []
   }
 
   componentDidMount() {
+    // fetch data & save it to stories
     fetch('/stories')
       .then(res => res.json())
       .then(stories => this.setState({
@@ -81,31 +47,43 @@ class StoriesTable extends React.Component {
     });
   }
 
+  detailsButton = (cell) => {
+    return (
+      <span
+        onClick={() => this.openModal(cell)}
+        style={{cursor: 'pointer'}}
+      >
+        Details
+      </span>
+    );
+  }
+
+  // table
+  columns = [{
+    dataField: 'objectID',
+    text: 'Id'
+  }, {
+    dataField: 'title',
+    text: 'Title'
+  }, {
+    dataField: 'author',
+    text: 'Author'
+  }, {
+    dataField: 'objectID',
+    text: 'Actions',
+    formatter: this.detailsButton
+  }];
+
   render () {
-    // table data
-    const stories = this.state.stories;
-
-    // table columns
-    const columns = [{
-      dataField: 'objectID',
-      text: 'Id'
-    }, {
-      dataField: 'title',
-      text: 'Title'
-    }, {
-      dataField: 'author',
-      text: 'Author'
-    }];
-
     return (
       this.state.isLoading ?
         <div>Loading ...</div> :
         <div>
           <BootstrapTable
             keyField='id'
-            data={stories}
-            columns={columns}
-            pagination={paginationFactory(options)}
+            data={this.state.stories}
+            columns={this.columns}
+            pagination={paginationFactory(paginationOptions)}
             bootstrap4={true}
           />
           <StoryModal
